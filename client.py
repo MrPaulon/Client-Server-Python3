aa0e7c7b9e7112b4cd7e2deaa7fb11c1dd774652
Paul <76972320+MrPaulon@users.noreply.github.com>
2023-02-22 14:49:21 +0100

    Ajout du threading et du renvoi de donnée coté serveur

--- client.py
+++ client.py
@@ -1,13 +1,16 @@
-# echo-client.py
-
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 12086)) # Connexion au serveur
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+print("Message à envoyer au serveur:")
+data = input(">> ")
+data = data.encode("utf8") # On encode le message
+s.sendall(data) # On envoie le message au serveur sous sa version codé
 
-print(f"Received {data!r}")
+# Le Serveur renvoit quelque chose
+
+data2 =''
+data2 = s.recv(1024)
+data2 = data2.decode("utf8") #On le decode car encodé coté serveur avant l'envoi
+print(data2) # On affiche le contenu de ce que nous avons reçu
--- server.py
+++ server.py
@@ -1,18 +1,40 @@
-# echo-server.py
-
 import socket
+import threading
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+class ClientThread(threading.Thread): #Définition de notre class, celle-ci correspondra à chaque client
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    def __init__(self, ip, port, clientsocket):
+
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.clientsocket = clientsocket
+        print("[+] Nouveau thread pour %s %s" % (self.ip, self.port, ))
+
+    def run(self):
+
+        print("Connexion de %s %s" % (self.ip, self.port, ))
+
+        r = self.clientsocket.recv(2048) # On récupère la date qui nous a été envoyé
+        r = r.decode("utf8") # On décode la data qui a été encodé juste avant son envoi
+        print(r)
+
+        # Data que nous allons envoyé au client
+
+        print("Que voulez-vous envoyer au client ", self.ip, self.port, " ?")
+        data =  input(">> ")
+        data = data.encode("utf8") # On encode la data avant son envoi
+        self.clientsocket.sendall(data) # On envoie la date au client correspondant
+
+        print("Client déconnecté...")
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind(("127.0.0.1",12086)) # On définit l'adresse ip et le port de notre serveur
+
+while True: # Cette boucle attends la connextion des clients et les créée selon la class définit plus haut
+    tcpsock.listen(10)
+    print( "En écoute...")
+    (clientsocket, (ip, port)) = tcpsock.accept()
+    newthread = ClientThread(ip, port, clientsocket) # Création d'un thrad réservé au client
+    newthread.start()
